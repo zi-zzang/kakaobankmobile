2d6231f1b827675a96aafef93148aa677fc69baf
zi-zzang <kjhwlgusdl@gmail.com>
2022-06-28 14:41:52 +0900

    기존 작업 모두 추가

---
+++ script/myjs.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('.btn_show_all_menu').click(function () {
+    $('.all_menu').slideDown();
+  });
+  $('.btn_hide_all_menu').click(function () {
+    $('.all_menu').slideUp();
+  });
+});
